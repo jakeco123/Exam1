2daa1b319a6ba2549f743c0d8feff34c9f27ad15
jcho40 <jcho40@csc208.csudh.edu>
2020-03-11 17:20:28 -0700

    exam1

---
+++ exam1.c
@@ -0,0 +1,78 @@
+//jcho.c
+//03/11/20
+//exam1
+//
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 20
+
+int question();
+int roll();
+
+	
+
+
+int main(void)
+{	
+	int arr,number;
+
+	srand(time(((NULL));
+ 	
+	question()
+	roll
+}
+
+int question()
+{
+	int number
+	printf("Enter the number of sides for your die(less than 20): \n");
+	scanf("%d",&d number);
+		return number;
+
+
+}
+int roll(int sides)
+{
+	int i,roll;
+	printf("Please enter the number of rolls \n");
+	scanf("%d",&d roll)
+	
+	for( i = 0, i<roll,i++)
+	{ 
+	rand () / sides +1;
+
+		
+
+	}
+}
+	
+int dice(int arr1)
+{
+	int count1,count2,count3,count4,count5,count6,count7,count8,count9,count10,count11,count12,count13
+
+	switch (arr1)
+	{
+		case 1:
+			arr1[0] = count1;
+				count1++;
+		case 2:
+			arr1[1] = count2;
+				count2++;
+		case 3: arr1[2] = count3;
+				count3++;
+		case 4: arr1[3]= count4;
+				count4++;
+		case 5: arr1[4]=count5;
+				count5++;
+		case 6: arr1[5]=count6;
+				count6++;
+		case 7: arr1[6]=count7;
+				count7++;
+		case 8: arr1[7]=count8;
+				count8++;
+		case 9: arr1[8]=count9;
+				count9++;
+	}
+}
